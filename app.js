6819a8c883c228ceb9d170217e441fa545561b5a
Bart Simpson <bartsimpson@Barts-MacBook-Pro.local>
2020-01-25 22:41:54 -0800

    committ all changes

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 function randomRGB() {
-    const r= Math.floor.apply(Math.random()*256);
-    const g= Math.floor.apply(Math.random()*256); 
-    const b= Math.floor.apply(Math.random()*256);
+    const r= Math.floor(Math.random()*256);
+    const g= Math.floor(Math.random()*256); 
+    const b= Math.floor(Math.random()*256);
     return `rgb(${r},${g},${b})`;
 }
 
@@ -10,4 +10,4 @@ const intervalId = setInterval(function() {
     for (let letter of letters ) {
         letter.style.color = randomRGB();
     }
-}, 2000) 
+}, 1000) 
