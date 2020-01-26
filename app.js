8a23fa3a0f38fbfbfd360d6035248abb0ed06fdf
Bart Simpson <bartsimpson@Barts-MacBook-Pro.local>
2020-01-25 22:16:29 -0800

    add v1 of color script

---
+++ app.js
@@ -0,0 +1,13 @@
+function randomRGB() {
+    const r= Math.floor.apply(Math.random()*256);
+    const g= Math.floor.apply(Math.random()*256); 
+    const b= Math.floor.apply(Math.random()*256);
+    return `rgb(${r},${g},${b})`;
+}
+
+const letters = document.querySelectorAll('.letter');
+const intervalId = setInterval(function() {
+    for (let letter of letters ) {
+        letter.style.color = randomRGB();
+    }
+}, 2000) 
