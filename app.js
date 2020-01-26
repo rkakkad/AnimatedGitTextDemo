34b2d09fdc9bdbeb3b87d14d98f3aef43ff948f7
Bart Simpson <bartsimpson@Barts-MacBook-Pro.local>
2020-01-25 23:01:07 -0800

    restrict colors to purples

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 function randomRGB() {
     const r= Math.floor.apply(Math.random()*256);
-    const g= Math.floor.apply(Math.random()*256); 
+    const g= 0; 
     const b= Math.floor.apply(Math.random()*256);
     return `rgb(${r},${g},${b})`;
 }
